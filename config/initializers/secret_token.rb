49b3d4484f0d23196b517c241b3101a06a84dd09
Duane Wittaker <dwittaker@gmail.com>
2013-12-15 11:43:31 -0500

    First commit - setup rails app and added subject update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheretolearn::Application.config.secret_token = '11f43335cc08d2d8e46550ef838ac5df2d1bc7c767a23805dd16ddde5a9288951e9efd45f946d17ff950407b134690ea5921b3de88aff5564fe08c3f8547acd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wheretolearn::Application.config.session_store :cookie_store, key: '_wheretolearn_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wheretolearn::Application.config.session_store :active_record_store
