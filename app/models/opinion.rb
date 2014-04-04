fef3f7c2079d1db9c18156b431fa9e461ee52ab6
Duane Wittaker <dwittaker@gmail.com>
2014-04-04 18:00:58 -0500

    Added source to against associated search with opinion and source type

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,6 +1,7 @@
 class Opinion < ActiveRecord::Base
   belongs_to :user, :inverse_of => :opinions
   attr_accessible :comment, :rating, :user, :optitle, :slug
+    delegate :username, :to => :user
 
   belongs_to :opinionable, :polymorphic => true
 
