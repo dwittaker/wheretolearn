ed09e6af0c1d818a4b5e492020feaef74f0f3441
Duane Wittaker <dwittaker@gmail.com>
2014-05-10 22:28:16 -0500

    seriously..... starting over migration, fixing module add class

--- db/migrate/20131221151949_add_moduleid_to_portfolio_1.rb
+++ db/migrate/20131221151949_add_moduleid_to_portfolio_1.rb
@@ -1,4 +1,4 @@
-class AddModuleidToPortfolio_1 < ActiveRecord::Migration
+class AddModuleidToPortfolio1 < ActiveRecord::Migration
   def change
     add_column :portfolios, :moduleid, :integer
   end
