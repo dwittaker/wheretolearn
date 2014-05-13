630c55e02061becec139be51ee4c0abb05fd64f0
Duane Wittaker <dwittaker@gmail.com>
2014-05-12 22:53:59 -0500

    added new line in migration to create second version of users_roles table which has its own id etc.

--- db/migrate/20140504011213_create_users_roles.rb
+++ db/migrate/20140504011213_create_users_roles.rb
@@ -1,5 +1,8 @@
 class CreateUsersRoles < ActiveRecord::Migration
   def change
+
+    rename_table :users_roles, :users_roles1
+
     create_table :users_roles do |t|
       t.integer :user_id
       t.integer :role_id
