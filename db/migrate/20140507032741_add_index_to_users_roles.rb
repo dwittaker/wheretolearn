8f8a5892b2e38dde322b672f0b1da145d5438b6b
Duane Wittaker <dwittaker@gmail.com>
2014-05-12 23:32:16 -0500

    removing the index from the old users_roles (1)

--- db/migrate/20140507032741_add_index_to_users_roles.rb
+++ db/migrate/20140507032741_add_index_to_users_roles.rb
@@ -1,6 +1,6 @@
 class AddIndexToUsersRoles < ActiveRecord::Migration
   def change
-    remove_index(:users_roles_1, name: 'index_users_roles_on_user_id_and_role_id')
+    remove_index(:users_roles1, name: 'index_users_roles_on_user_id_and_role_id')
 
     add_index :users_roles, [:user_id,:role_id], :unique => true
   end
