45a8d9261f4dbfd2d2c6134d6e9f9524b0fab4dd
Duane Wittaker <dwittaker@gmail.com>
2014-05-22 06:46:56 -0500

    fixed search controller to allow for two different search field names since the main search was modified to 'searchqrybig' to allow separate styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,17 @@
 class SearchController < ApplicationController
   def search
 
-    if params[:srchquery]
-     @searchables = PgSearch.multisearch(params[:srchquery])
+    if params[:srchquerybig]
+     @searchables = PgSearch.multisearch(params[:srchquerybig])
       #@portfolios = Portfolio.search_by_link(params[:srchquery])
     else
+      if params[:srchquery]
+        @searchables = PgSearch.multisearch(params[:srchquery])
+        #@portfolios = Portfolio.search_by_link(params[:srchquery])
+      else
+
+        #@searchables = null
+      end
       #@searchables = null
     end
 
