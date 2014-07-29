bf9cdfe80543ae9b564dba9ae40aeeee8c1ce0ba
Duane Wittaker <dwittaker@gmail.com>
2014-07-28 22:59:33 -0500

    omniauth & login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140721015303) do
+ActiveRecord::Schema.define(:version => 20140729035711) do
 
   create_table "blog_comments", :force => true do |t|
     t.string   "name",       :null => false
@@ -171,6 +171,16 @@ ActiveRecord::Schema.define(:version => 20140721015303) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sources", :force => true do |t|
     t.string   "name"
