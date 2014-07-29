125567fa513c0ccaf4aede444a34d49b103a5746
Duane Wittaker <dwittaker@gmail.com>
2014-07-28 21:45:20 -0500

    omniauth & login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"], "facebook")
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
@@ -12,4 +12,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def twitter
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"], "twitter")
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -98,19 +98,31 @@ class User < ActiveRecord::Base
     "http://gravatar.com/avatar/#{hash}?d=mm"
   end
 
-  def self.from_omniauth(auth)
+  def self.from_omniauth(auth, authprovider)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.first_name   # assuming the user model has a first name
-      user.last_name = auth.info.last_name   # assuming the user model has a last name
-      user.image = auth.info.image # assuming the user model has an image
+
+      if authprovider == "facebook" then
+        user.first_name = auth.info.first_name
+        user.last_name = auth.info.last_name
+      end
+
+      if authprovider == "twitter" then
+        user.profile_name = auth.info.nickname
+      end
+
+
+      #user.image = auth.info.image
     end
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      if data == session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+      if data == session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
     end
