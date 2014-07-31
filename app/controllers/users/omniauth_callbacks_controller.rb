3a63c1fefa2861cfc4d93df290aaae814371769c
Duane Wittaker <dwittaker@gmail.com>
2014-07-30 20:13:15 -0500

    omniauth & login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #@user = User.from_omniauth(request.env["omniauth.auth"], "facebook")
     @user = User.find_for_oauth(request.env["omniauth.auth"], "facebook")
 
+
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
