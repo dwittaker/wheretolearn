334107d4770be3f50878af80ab95068a9d9831f6
Duane Wittaker <dwittaker@gmail.com>
2014-07-31 20:52:21 -0500

    omniauth & login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -19,15 +19,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #@user = User.from_omniauth(request.env["omniauth.auth"], "twitter")
     @user = User.find_for_oauth(request.env["omniauth.auth"], "twitter")
 
-
+log_test('here')
 
 
     if defined? @user && @user.persisted?
+      log_test('here inside')
       sign_in_and_redirect @user #,  :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     else
+      log_test('here in else')
       session["devise.twitter_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+
+    log_test('here after block')
   end
 end
