8ef4b8022bb243d96872e9b1e607bde909b93ef2
Duane Wittaker <dwittaker@gmail.com>
2014-07-31 22:52:26 -0500

    omniauth & login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
     if defined? @user && @user.persisted?
-      sign_in_and_redirect @user#, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
@@ -19,22 +19,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #@user = User.from_omniauth(request.env["omniauth.auth"], "twitter")
     @user = User.find_for_oauth(request.env["omniauth.auth"], "twitter")
 
-log_test('here')
-
 
     if defined? @user && @user.persisted?
-      log_test('here inside with')
-      log_test(defined? @user)
-      log_test(@user.persisted?)
-      sign_in_and_redirect @user #,  :event => :authentication #this will throw if @user is not activated
+
+      sign_in_and_redirect @user ,  :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     else
-      log_test('here in else')
+
       session["devise.twitter_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
 
-    log_test('here after block')
   end
 
   def log_test(message)
